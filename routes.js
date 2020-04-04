69c45b92e5c5f9974e6312a3b843a81f09bc0618
albasychris <albasychris@gmail.com>
2020-04-05 09:32:07 +1200

    complete display of services per customer

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require("express");
 
-const db = require('./db')
+const db = require("./db");
 
-const router = express.Router()
+const router = express.Router();
 
-module.exports = router
+module.exports = router;
 
 // // TEST
 // router.get('/foo', (req, res) => {
@@ -12,54 +12,61 @@ module.exports = router
 // })
 
 // GET Home page which displays list of customers
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   db.getCustomers()
-    .then(customers => {
-      res.render('home', { customers })
+    .then((customers) => {
+      res.render("home", { customers });
     })
-    .catch(err => {
-      res.send('Error: ' + err.message)
-    })
-})
+    .catch((err) => {
+      res.send("Error: " + err.message);
+    });
+});
 
 // GET Customer page which displays a customer's information by id & the services associated
-router.get('/customer/:id', (req, res) => {
-  const id = Number(req.params.id)
-  db.getServices(id)
-    .then(result => {
-      const viewData = {
-        customerId: id,
-        name: result.name,
-        phone: result.phone,
-        email: result.email,
-        address: result.address,
-        serviceId: result.serviceId,
-        description: result.description,
-        engineer: result.engineer,
-        hours: result.hours,
-        materials: result.materials,
-        material_cost: result.material_cost
-      }
-      res.render('customer', viewData)
-    })
-})
+router.get("/customer/:id", (req, res) => {
+  const id = Number(req.params.id);
+  db.getServices(id).then((result) => {
+    console.log(result);
+    console.log(result[0]);
+
+    const viewData = {
+      customerId: result[0].customerId,
+      name: result[0].name,
+      phone: result[0].phone,
+      email: result[0].email,
+      address: result[0].address,
+      services: [],
+    };
+
+    for (let i = 0; i < result.length; i++) {
+      viewData.services.push({
+        serviceId: result[i].serviceId,
+        description: result[i].description,
+        engineer: result[i].engineer,
+        hours: result[i].hours,
+        materials: result[i].materials,
+        material_cost: result[i].material_cost,
+      });
+    }
+
+    res.render("customer", viewData);
+  });
+});
 
 // GET Add service page
-router.get('/add/:id', (req, res) => {
-  db.getCustomer(Number(req.params.id))
-    .then((customer) => {
-      const viewData = {
-        id: req.params.id,
-        name: customer.name
-      }
-      res.render('add', viewData)
-    })
-})
+router.get("/add/:id", (req, res) => {
+  db.getCustomer(Number(req.params.id)).then((customer) => {
+    const viewData = {
+      id: req.params.id,
+      name: customer.name,
+    };
+    res.render("add", viewData);
+  });
+});
 
 // POST Post service to database
-router.post('/add/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const serviceData = req.body
-  db.addService(serviceData, id)
-    .then(() => res.redirect(`/customer/${id}`))
-})
+router.post("/add/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const serviceData = req.body;
+  db.addService(serviceData, id).then(() => res.redirect(`/customer/${id}`));
+});
